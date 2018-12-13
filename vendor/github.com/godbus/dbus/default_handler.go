6406bcd34c47bd5e6e4db10b0cd65afc0caca0c5
edulop <elopez@chanzuckerberg.com>
2018-12-13 13:47:11 -0800

    Vendor stuff

--- vendor/github.com/godbus/dbus/default_handler.go
+++ vendor/github.com/godbus/dbus/default_handler.go
@@ -18,12 +18,7 @@ func newIntrospectIntf(h *defaultHandler) *exportedIntf {
 	return newExportedIntf(methods, true)
 }
 
-//NewDefaultHandler returns an instance of the default
-//call handler. This is useful if you want to implement only
-//one of the two handlers but not both.
-//
-// Deprecated: this is the default value, don't use it, it will be unexported.
-func NewDefaultHandler() *defaultHandler {
+func newDefaultHandler() *defaultHandler {
 	h := &defaultHandler{
 		objects:     make(map[ObjectPath]*exportedObj),
 		defaultIntf: make(map[string]*exportedIntf),
@@ -163,7 +158,6 @@ func newExportedObject() *exportedObj {
 }
 
 type exportedObj struct {
-	mu         sync.RWMutex
 	interfaces map[string]*exportedIntf
 }
 
@@ -171,27 +165,19 @@ func (obj *exportedObj) LookupInterface(name string) (Interface, bool) {
 	if name == "" {
 		return obj, true
 	}
-	obj.mu.RLock()
-	defer obj.mu.RUnlock()
 	intf, exists := obj.interfaces[name]
 	return intf, exists
 }
 
 func (obj *exportedObj) AddInterface(name string, iface *exportedIntf) {
-	obj.mu.Lock()
-	defer obj.mu.Unlock()
 	obj.interfaces[name] = iface
 }
 
 func (obj *exportedObj) DeleteInterface(name string) {
-	obj.mu.Lock()
-	defer obj.mu.Unlock()
 	delete(obj.interfaces, name)
 }
 
 func (obj *exportedObj) LookupMethod(name string) (Method, bool) {
-	obj.mu.RLock()
-	defer obj.mu.RUnlock()
 	for _, intf := range obj.interfaces {
 		method, exists := intf.LookupMethod(name)
 		if exists {
@@ -228,15 +214,8 @@ func (obj *exportedIntf) isFallbackInterface() bool {
 	return obj.includeSubtree
 }
 
-//NewDefaultSignalHandler returns an instance of the default
-//signal handler. This is useful if you want to implement only
-//one of the two handlers but not both.
-//
-// Deprecated: this is the default value, don't use it, it will be unexported.
-func NewDefaultSignalHandler() *defaultSignalHandler {
-	return &defaultSignalHandler{
-		closeChan: make(chan struct{}),
-	}
+func newDefaultSignalHandler() *defaultSignalHandler {
+	return &defaultSignalHandler{}
 }
 
 func isDefaultSignalHandler(handler SignalHandler) bool {
@@ -246,9 +225,8 @@ func isDefaultSignalHandler(handler SignalHandler) bool {
 
 type defaultSignalHandler struct {
 	sync.RWMutex
-	closed    bool
-	signals   []chan<- *Signal
-	closeChan chan struct{}
+	closed  bool
+	signals []chan<- *Signal
 }
 
 func (sh *defaultSignalHandler) DeliverSignal(intf, name string, signal *Signal) {
@@ -258,35 +236,19 @@ func (sh *defaultSignalHandler) DeliverSignal(intf, name string, signal *Signal)
 		return
 	}
 	for _, ch := range sh.signals {
-		select {
-		case ch <- signal:
-		case <-sh.closeChan:
-			return
-		default:
-			go func() {
-				select {
-				case ch <- signal:
-				case <-sh.closeChan:
-					return
-				}
-			}()
-		}
+		ch <- signal
 	}
 }
 
 func (sh *defaultSignalHandler) Init() error {
 	sh.Lock()
 	sh.signals = make([]chan<- *Signal, 0)
-	sh.closeChan = make(chan struct{})
 	sh.Unlock()
 	return nil
 }
 
 func (sh *defaultSignalHandler) Terminate() {
 	sh.Lock()
-	if !sh.closed {
-		close(sh.closeChan)
-	}
 	sh.closed = true
 	for _, ch := range sh.signals {
 		close(ch)
